e2ef8d57ad1c8b98d0d09e519a85abb2427c65f2
Angel <aleangrodriguez123@gmail.com>
2016-06-24 12:29:03 -0500

    Añadido filtro en el modal de filtro de clientes para cuando es administrador

--- client/templates/facturacion/filtros/clientesHoras.js
+++ client/templates/facturacion/filtros/clientesHoras.js
@@ -5,7 +5,8 @@ Template.filtroClienteHoraModal.onCreated(function () {
     let self  = this;
     let bufeteId = Meteor.user().profile.bufeteId;
     self.autorun(function () {
-        self.subscribe('clientesxasuntosxmiembro',bufeteId,Meteor.userId())
+        if(Meteor.user().profile.roles.bufete[0]!="administrador") return self.subscribe('clientesxasuntosxmiembro',bufeteId,Meteor.userId())
+        self.subscribe('clientes',bufeteId);
     })
 
 })
---
+++ client/templates/facturacion/filtros/miembrosHoras.js
@@ -0,0 +1,17 @@
+Template.filtroMiembroHoraModal.onCreated(function () {
+
+})
+
+Template.filtroMiembroHoraModal.onRendered(function () {
+
+})
+
+Template.filtroMiembroHoraModal.events({
+
+})
+
+Template.filtroMiembroHoraModal.helpers({
+    miembros(){
+        
+    }
+})
