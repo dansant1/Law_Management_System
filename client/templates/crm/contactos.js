f0509a1bde58cfb68f75b798c2f8bd59c0298d99
Daniel Delgadillo <danieldelgadilloh@gmail.com>
2016-07-05 00:19:29 -0500

    Agregamos vista para ver a mas detalle un contacto y las negociaciones en el modulo crm

--- client/templates/crm/contactos.js
+++ client/templates/crm/contactos.js
@@ -1,3 +1,16 @@
 Template.listaContactosCRM.onCreated(function () {
-	
+	var self = this;
+
+	self.autorun(function() {
+
+		let bufeteId = Meteor.user().profile.bufeteId;
+
+    	self.subscribe('clientes', bufeteId);
+   });
+});
+
+Template.listaContactosCRM.helpers({
+	clientes() {
+		return Clientes.find({});
+	}
 });
