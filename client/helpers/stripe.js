7061f14e7dcc22de88aae34cc11a2fa4fff28bf2
Daniel Delgadillo <danieldelgadilloh@gmail.com>
2016-08-01 21:01:19 -0500

    Se configuro api keys de stripe en los settings

---
+++ client/helpers/stripe.js
@@ -0,0 +1,30 @@
+Meteor.startup( function() {
+
+	var stripeKey = Meteor.settings.public.stripe.testPublishableKey;
+	
+  	Stripe.setPublishableKey( stripeKey );
+
+  	STRIPE = {
+
+    		getToken: function( domElement, card, callback ) {
+      		
+      		Stripe.card.createToken( card, function( status, response ) {
+        			
+        			if ( response.error ) {
+          				Bert.alert( response.error.message, "danger" );
+        			} else {
+          				STRIPE.setToken( response.id, domElement, callback );
+        			}
+
+      		});
+
+    		},
+    		setToken: function( token, domElement, callback ) {
+      		
+      		$( domElement ).append( $( "<input type='hidden' name='stripeToken' />" ).val( token ) );
+      		callback();
+    		
+    		}
+
+  	};
+});
